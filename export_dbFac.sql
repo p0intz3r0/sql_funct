6fdba8ddeda92ef6b629d2f438383055237d19d8
p0intz3r0 <p0intz3r0@p0intz3r0s-MacBook-Pro.local>
2017-04-06 10:46:24 +0200

    Revert "Various useful sql query files"
    This reverts commit 064ac4629e21c083e22c08ac53a85374839be69b.

--- export_dbFac.sql
+++
@@ -1,6 +0,0 @@
- 
-SELECT  c_invbooks.invoice_number, REPLACE(REPLACE(c_invbooks.type,'INVOICE','FATO'),'CREDIT_NOTE', 'AATO'),c_invbooks_lines.order_title,  c_invbooks.tiers_code_display, c_invbooks.inv_date, c_invbooks.period, c_invbooks.type_prestation, c_invbooks_lines.ht_unit, c_invbooks_lines.unit, c_invbooks.prestation_ht, c_invbooks.montant_ttc, c_invbooks.pay_status, REPLACE(c_invbooks_lines.order_code,'-',''), c_invbooks_lines.descr, datediff(c_invbooks.due_date ,c_invbooks.inv_date), c_invbooks_lines.user_id
-FROM c_invbooks
-LEFT JOIN c_invbooks_billing ON c_invbooks.invoice_id = c_invbooks_billing.invoice_id
-                LEFT JOIN c_invbooks_lines ON c_invbooks.invoice_id = c_invbooks_lines.invoice_id
-                               WHERE c_invbooks.invoice_number > 1702006; /* Numero de facture a partir duquel la requete se fait */
--- export_to_factor.sql
+++
@@ -1,7 +0,0 @@
-
-SELECT (@type:= c_invbooks.`type`) AS ftype, REPLACE(REPLACE(c_invbooks.`type`, 'INVOICE', 'F'), 'CREDIT_NOTE', 'A'), c_invbooks.`invoice_number`, c_invbooks.`inv_date`, c_invbooks.`tiers_code_display`,
-c_invbooks.`prestation_ht`, c_invbooks.`montant_ttc`, IF(@type= 'INVOICE',datediff(c_invbooks.`due_date`, c_invbooks.`inv_date`), NULL), IF(@type='INVOICE',  c_invbooks.`due_date`, NULL), IF(@type='INVOICE',REPLACE(c_invbooks_billing.`billing_medcode`, 'FACTOR', 'VIR'), NULL)
-FROM c_invbooks
-LEFT JOIN c_invbooks_billing ON c_invbooks.`invoice_id` = c_invbooks_billing.`invoice_id`
-WHERE c_invbooks_billing.`billing_medcode` = 'FACTOR'
-AND c_invbooks.`invoice_number` > 1703000;
